325574b1f5a0f60fece4a879a8057d198b0a1290
Becky Pollard <hello@beckypollard.com>
2018-07-03 20:19:20 -0400

    Finished up main.js, styling, added favicon

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,24 @@
-
 function playsound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const cat = document.getElementById("tomcat").src;
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     key.classList.add("playing");
+    document.getElementById("tomcat").src="./images/drum2.jpg";
+    console.log("oh god");
 }
 function removeTransition(e) {
     if(e.propertyName !== "transform") return;
     this.classList.remove("playing");
+}
+function cat(e){
+    document.getElementById("tomcat").src="./images/drum1.jpg";
 }
 
 const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
 window.addEventListener("keydown", playsound);
+window.addEventListener("keyup", cat);
